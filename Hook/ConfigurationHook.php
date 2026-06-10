e9334c4959c23efcc6120f4871ccbfb9e0596726
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-10 17:38:30 +0200

    feat: migrate and modernize back-office for Twig

--- Hook/ConfigurationHook.php
+++ Hook/ConfigurationHook.php
@@ -2,25 +2,47 @@
 
 namespace ReplacedOrderModule\Hook;
 
+use ReplacedOrderModule\Form\ReplacedOrderModuleForm;
+use ReplacedOrderModule\Form\SimpleReplacedOrderModuleForm;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Form\TheliaFormFactory;
 use Thelia\Core\Hook\BaseHook;
+use Thelia\Core\Template\Parser\ParserResolver;
 
 class ConfigurationHook extends BaseHook
 {
+    public function __construct(
+        private readonly TheliaFormFactory $formFactory,
+        ?EventDispatcherInterface $dispatcher = null,
+        ?ParserResolver $parserResolver = null,
+    ) {
+        parent::__construct($dispatcher, $parserResolver);
+    }
+
     public function onModuleConfiguration(HookRenderEvent $event): void
     {
-        $event->add(($this->render("module_configuration.html")));
+        $simpleForm = $this->formFactory->createForm(SimpleReplacedOrderModuleForm::getName());
+        $simpleForm->createView();
+
+        $replacedForm = $this->formFactory->createForm(ReplacedOrderModuleForm::getName());
+        $replacedForm->createView();
+
+        $event->add($this->render('ReplacedOrderModule/module_configuration.html.twig', [
+            'simpleForm' => $simpleForm->getView(),
+            'replacedForm' => $replacedForm->getView(),
+        ]));
     }
 
     public static function getSubscribedHooks(): array
     {
         return [
-            "module.configuration" => [
+            'module.configuration' => [
                 [
-                    "type" => "back",
-                    "method" => "onModuleConfiguration"
+                    'type' => 'back',
+                    'method' => 'onModuleConfiguration',
                 ],
-            ]
+            ],
         ];
     }
 }
